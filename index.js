dac01618cfad7dd02204062964ea224afcab33d3
Rajan <rajan.kerai@gmail.com>
2018-04-01 17:16:10 +0100

    2nd commit!

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log('index.js excecuting');
+var express = require('express')
+const status = require('http-status');
+var app = express(); 
+
+app.get('/',function(req,res){
+	res.send('Hello, World!');
+});
+
+app.post('/',function(req,res){
+	res.send('Hello, World, POSTED!!');
+});
+
+var port = 3000;
+
+app.listen(port,function(){
+	console.log('Listening on port ' + port);
+});
